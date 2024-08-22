be8aa96e77c8f5230392b87f0e2368125b359bd9
Ey Pacha! <pachaguionbajo@gmail.com>
2024-08-22 15:02:35 -0300

    styles: general styles

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,9 +1,9 @@
 <template>
     <div class="flex h-dvh flex-col">
-        <div class="container h-full max-w-screen-sm">
+        <div class="container h-full max-w-screen-sm py-8">
             <slot class="mx-auto" />
         </div>
-        <div class="shadow-top">
+        <div class="bg-white shadow-top">
             <div class="container max-w-screen-sm p-4">
                 <div class="flex w-full gap-5">
                     <NavButton to="/list" icon="list">All</NavButton>
