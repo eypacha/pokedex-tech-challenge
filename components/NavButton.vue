3a6114215b92efce8ea328494e5b4c4358dec8d1
Ey Pacha! <pachaguionbajo@gmail.com>
2024-08-22 13:59:37 -0300

    Chore: NavButton component

---
+++ components/NavButton.vue
@@ -0,0 +1,23 @@
+<template>
+    <NuxtLink :to="props.to" class="flex-1">
+        <Button :variant="getButtonVariant()" class="w-full">
+            <fa v-if="props.icon" :icon="props.icon" class="mr-2" />
+            <slot />
+        </Button>
+    </NuxtLink>
+</template>
+<script setup lang="ts">
+import { useRoute } from 'vue-router'
+const route = useRoute()
+
+interface Props {
+    to: string
+    icon?: string
+}
+
+const props = defineProps<Props>()
+
+const getButtonVariant = () => {
+    return route.path == props.to ? 'default' : 'outline'
+}
+</script>
--- layouts/default.vue
+++ layouts/default.vue
@@ -4,35 +4,12 @@
             <slot class="mx-auto" />
         </div>
         <div class="shadow-top">
-            <div class="container flex max-w-screen-sm gap-5 p-4">
+            <div class="container max-w-screen-sm p-4">
                 <div class="flex w-full gap-5">
-                    <NuxtLink to="/list" class="flex-1">
-                        <Button
-                            :variant="getButtonVariant('/list')"
-                            class="w-full"
-                        >
-                            <fa icon="list" class="mr-2" /> All
-                        </Button>
-                    </NuxtLink>
-                    <NuxtLink to="/favorites" class="flex-1">
-                        <Button
-                            :variant="getButtonVariant('/favorites')"
-                            class="w-full"
-                        >
-                            <fa icon="star" class="mr-2" /> Favorites
-                        </Button>
-                    </NuxtLink>
+                    <NavButton to="/list" icon="list">All</NavButton>
+                    <NavButton to="/favorites" icon="star">Favorites</NavButton>
                 </div>
             </div>
         </div>
     </div>
 </template>
-
-<script setup lang="ts">
-import { useRoute } from 'vue-router'
-const route = useRoute()
-
-const getButtonVariant = (routePath: string) => {
-    return route.path == routePath ? 'default' : 'outline'
-}
-</script>
