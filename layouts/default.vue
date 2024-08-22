d334fc4e4386d729f9daabee4ca9bb856f903cc9
Ey Pacha! <pachaguionbajo@gmail.com>
2024-08-21 23:37:54 -0300

    feat: default layout and navigation

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,38 @@
 <template>
-        <slot/>
-        <NuxtLink to="/list">All</NuxtLink>
-        <NuxtLink to="/favorites">Favorites</NuxtLink>
-        
+    <div class="flex h-dvh flex-col">
+        <div class="container h-full max-w-screen-sm">
+            <slot class="mx-auto" />
+        </div>
+        <div class="shadow-top">
+            <div class="container flex max-w-screen-sm gap-5 p-4">
+                <div class="flex w-full gap-5">
+                    <NuxtLink to="/list" class="flex-1">
+                        <Button
+                            :variant="getButtonVariant('/list')"
+                            class="w-full"
+                        >
+                            <fa icon="list" class="mr-2" /> All
+                        </Button>
+                    </NuxtLink>
+                    <NuxtLink to="/favorites" class="flex-1">
+                        <Button
+                            :variant="getButtonVariant('/favorites')"
+                            class="w-full"
+                        >
+                            <fa icon="star" class="mr-2" /> Favorites
+                        </Button>
+                    </NuxtLink>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
+
+<script setup lang="ts">
+import { useRoute } from 'vue-router'
+const route = useRoute()
+
+const getButtonVariant = (routePath: string) => {
+    return route.path == routePath ? 'default' : 'outline'
+}
+</script>
