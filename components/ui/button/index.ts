52c81d0b16d255a63efff24a18881abe544c2f2f
Ey Pacha! <pachaguionbajo@gmail.com>
2024-08-21 21:32:57 -0300

    feat: Button component

---
+++ components/ui/button/index.ts
@@ -0,0 +1,30 @@
+import { type VariantProps, cva } from 'class-variance-authority'
+
+export { default as Button } from './Button.vue'
+
+export const buttonVariants = cva(
+  'inline-flex items-center justify-center whitespace-nowrap rounded-full text-lg font-bold ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none',
+  {
+    variants: {
+      variant: {
+        default: 'bg-primary text-white hover:bg-primary-dark disabled:bg-grey-light disabled:text-white',
+        outline:
+          'border border-primary text-primary bg-background hover:text-primary-dark hover:border-primary-dark disabled:text-grey-light disabled:border-grey-light',
+        link: 'text-primary underline-offset-4 hover:underline',
+      },
+      size: {
+        default: 'h-11 px-4 py-2',
+        xs: 'h-7 px-2 text-xs',
+        sm: 'h-9 px-3 text-sm',
+        lg: 'h-12 px-8',
+        icon: 'h-10 w-10',
+      },
+    },
+    defaultVariants: {
+      variant: 'default',
+      size: 'default',
+    },
+  },
+)
+
+export type ButtonVariants = VariantProps<typeof buttonVariants>
