2b769b472d1a00ea6095a1747c0189f7c00df129
Ey Pacha! <pachaguionbajo@gmail.com>
2024-08-23 04:06:34 -0300

    style: disabled select text on buttons

--- components/ui/button/index.ts
+++ components/ui/button/index.ts
@@ -3,7 +3,7 @@ import { type VariantProps, cva } from 'class-variance-authority'
 export { default as Button } from './Button.vue'
 
 export const buttonVariants = cva(
-  'inline-flex items-center justify-center whitespace-nowrap rounded-full text-lg font-bold ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none',
+  'inline-flex items-center justify-center whitespace-nowrap rounded-full text-lg font-bold ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 select-none disabled:pointer-events-none',
   {
     variants: {
       variant: {
