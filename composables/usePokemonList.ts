55461e4b0b76dd988984fc443a2b380e8cf39bdd
Ey Pacha! <pachaguionbajo@gmail.com>
2024-08-23 00:55:20 -0300

    Refactor: Simplify usePokemonList to return basic Pokémon list with status

--- composables/usePokemonList.ts
+++ composables/usePokemonList.ts
@@ -1,23 +1,9 @@
 export function usePokemonList() {
-    const { data: pokemons, error } = useFetch('https://pokeapi.co/api/v2/pokemon', {
-      query: { limit: -1 },
-      transform: (response: { results: { name: string; url: string }[] }) => {
-        return response.results.map(pokemon => {
-          const formatedName = pokemon.name
-            .split('-')
-            .map(word => word.charAt(0).toUpperCase() + word.slice(1))
-            .join(' ');
-  
-          const id = pokemon.url.split('/').filter(Boolean).pop();
-  
-          return {
-            name: formatedName,
-            id: Number(id)
-          };
-        });
-      }
+    const { status, data: pokemons, error } = useFetch('https://pokeapi.co/api/v2/pokemon/', {
+      query: { limit: 9999 },
+      transform: (response: { results: { name: string }[] }) => 
+        response.results.map(({ name }) => ({ name }))
     });
   
-    return { pokemons, error };
+    return { status, pokemons, error };
   }
-  
