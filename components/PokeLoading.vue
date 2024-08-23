bba65e2aa73f44a5e5519b94a8b04d73e27e3d31
Ey Pacha! <pachaguionbajo@gmail.com>
2024-08-23 00:57:22 -0300

    Add: Loading spinner component with Pokeball animation

---
+++ components/PokeLoading.vue
@@ -0,0 +1,5 @@
+<template>
+    <div class="fixed inset-0 flex items-center justify-center bg-white z-50">
+      <img src="~/assets/img/pokeball.svg" alt="Loading..." class="animate-bounce" />
+    </div>
+</template>
